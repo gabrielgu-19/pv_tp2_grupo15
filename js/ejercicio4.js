7be22ea6662fa65431f09015c4a6cdfe4fccaeb2
Joel <yoel22cii@gmail.com>
2026-04-24 11:11:57 -0300

    Agrego ejercicio 4: calcular el area y perimetro de un rectangulo. Cespedes Joel Normando

---
+++ js/ejercicio4.js
@@ -0,0 +1,8 @@
+const base = 15, altura = 20;
+const area = base * altura;
+const perimetro = 2 * (base + altura);
+
+console.log("El área del rectángulo es: " + area + "cm²");
+console.log("El perímetro del rectángulo es: " + perimetro + "cm");
+
+
