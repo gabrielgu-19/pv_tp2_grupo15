ad966df6868dfbb1181d71c3162496f252c2dde3
Joel <yoel22cii@gmail.com>
2026-04-30 11:00:23 -0300

    Agrego ejercicio 23: Evento con input y cambiar color de fondo. Cespedes Joel Normando

---
+++ js/funciones23.js
@@ -0,0 +1,7 @@
+export const mostrar_texto = (texto_ingresado, texto_mostrar) => {
+    texto_mostrar.textContent = texto_ingresado.value;
+}
+
+export const cambiar_fondo = (parrafo) => {
+    parrafo.style.backgroundColor = "#4dff4d";
+}
